b538dcf8dfa6d404c1c59899a2c9d32e0d57c1e3
Jacob Albright <jacob.albright23@gmail.com>
2019-03-24 18:00:18 -0500

    updated frontend to be served

---
+++ app/templates/precache-manifest.779dd8fb50f585f50a42cac8789101ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4332a3088a0ffc82faf",
+    "url": "/static/css/app.05cf53ae.css"
+  },
+  {
+    "revision": "e4332a3088a0ffc82faf",
+    "url": "/static/js/app.5b2627e9.js"
+  },
+  {
+    "revision": "03bf365c10b53cba29bb",
+    "url": "/static/css/chunk-vendors.84e22b27.css"
+  },
+  {
+    "revision": "03bf365c10b53cba29bb",
+    "url": "/static/js/chunk-vendors.51072250.js"
+  },
+  {
+    "revision": "48ffeed2b757eee742b4f36d984b5616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
